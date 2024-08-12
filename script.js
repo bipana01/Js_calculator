67c0c51c7b32385d20fe4d553830300d5b9b3ca6
bipana01 <119509836+bipana01@users.noreply.github.com>
2024-08-12 14:55:08 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+let firstNumber =parseInt(prompt("Enter a first number:"))
+let secondNumber = parseInt(prompt("Enter a second number:"))
+let operation = prompt("Enter a operation:")
+
+
+if (operation == "+") {
+    console.log(firstNumber + secondNumber)
+}else if (operation == "-")
+{
+    console.log(firstNumber - secondNumber)
+}else if (operation == "*")
+    {
+        console.log(firstNumber * secondNumber)
+    }else if (operation == "/")
+        {
+            console.log(firstNumber / secondNumber)
+        } else if (operation == "%")
+            {
+                console.log(firstNumber % secondNumber)
+            }
+                   
+    
+
+    
+    
+
+
